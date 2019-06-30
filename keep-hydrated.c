37e153437e6caab52a4a0f5291e44c62feb3f711
Ellis Lee <ellismckenzielee@gmail.com>
2019-06-30 18:01:43 +0100

    completed keep-hydrated kata

---
+++ keep-hydrated.c
@@ -0,0 +1,7 @@
+/*KEEP HYDRATED KATA*/
+/*Function returns the number of litres required to keep hydrated,
+based on the time of activity */
+
+int Liters(double time) {
+  return floor(time*0.5);
+}
