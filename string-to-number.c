02c52e6c23065d359895d6237918737d5df48415
Ellis Lee <ellismckenzielee@gmail.com>
2019-06-30 18:11:33 +0100

    completed string-t0-number kata

---
+++ string-to-number.c
@@ -0,0 +1,8 @@
+/*CONVERT STRING TO A NUMBER KATA*/
+/*Function converts a string to a number and returns this */
+
+int string_to_number(const char *src) {
+  int num;
+  num = atoi(src);
+  return num;
+}
